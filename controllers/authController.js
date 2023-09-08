43764deee3657ec17b5c16320b6d15a32b731c5e
Ömer Yargı <yarginomerfaruk@gmail.com>
2023-09-08 11:15:59 +0300

    login page OK

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
-// Yeni kurs oluşturmak için kullanılır.
+// Yeni kullanıcı oluşturmak için kullanılır.
 exports.createUser = async (req, res) => {
     const user = await User.create(req.body);
     try {
@@ -8,6 +9,31 @@ exports.createUser = async (req, res) => {
             status: 'success',
             user,
         });
+    } catch (error) {
+        res.status(400).json({
+            status: 'failed',
+            error,
+        });
+    }
+};
+
+// Eskiden oluşturulmuş kullanıcı var mı yok mu. Giriş sayfası
+exports.loginUser = async (req, res) => {
+    try {
+        // Form üzerinden verileri alıyoruz
+        const { email, password } = req.body;
+        // Form üzerinden gelen mail adresi var mı yok mu kontrol ediyoruz.
+        const user = await User.findOne({ email });
+        if (!user) {
+            res.status(400).send('kullanici yok');
+        }
+        // Database üzerinden gelen user.password ile form üzerinden gelen password karşılaştırması yapılıyor.
+        const same = await bcrypt.compare(password, user.password);
+        if (same) {
+            res.status(200).send('login');
+        } else {
+            res.status(400).send('Şifre Hatalı');
+        }
     } catch (error) {
         res.status(400).json({
             status: 'failed',
--- controllers/pageController.js
+++ controllers/pageController.js
@@ -3,18 +3,25 @@ exports.getIndexPage = (req, res) => {
     res.status(200).render('index', {
         page_name: 'index',
     });
-}
+};
 
 // ABOUT PAGE ROUTE
 exports.getAboutPage = (req, res) => {
     res.status(200).render('about', {
         page_name: 'about',
     });
-}
+};
 
-// ABOUT PAGE ROUTE
+// SIGNUP PAGE ROUTE
 exports.getRegisterPage = (req, res) => {
     res.status(200).render('register', {
         page_name: 'register',
     });
-}
+};
+
+// Login PAGE ROUTE
+exports.getLoginPage = (req, res) => {
+    res.status(200).render('login', {
+        page_name: 'login',
+    });
+};
